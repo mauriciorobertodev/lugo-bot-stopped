72af2e897afa01109296957d6e1d187b5cdf44bb
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-09-21 19:05:30 -0300

    concerto do bug sem as variáveis de ambiente

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -7,28 +7,24 @@ class Config {
     private move_to_expected_position = [];
 
     constructor() {
-        if (process.env.TRY_CATCH_BALL === "all") this.try_catch_the_ball_all = true;
-        if (process.env.MOVE_IN_BALL_DIRECTION === "all") this.move_in_ball_direction_all = true;
-        if (process.env.MOVE_TO_EXPECTED_POSITION === "all") this.move_to_expected_position_all = true;
+        const tryCatchBall = process.env.TRY_CATCH_BALL;
+        const moveInBallDirection = process.env.MOVE_IN_BALL_DIRECTION;
+        const moveToExpectedPosition = process.env.MOVE_TO_EXPECTED_POSITION;
 
-        if (process.env.TRY_CATCH_BALL != "none" && process.env.TRY_CATCH_BALL != "all") this.try_catch_the_ball = process.env.TRY_CATCH_BALL.split(",").map((n) => parseInt(n));
-        if (process.env.MOVE_IN_BALL_DIRECTION != "none" && process.env.MOVE_IN_BALL_DIRECTION != "all") this.move_in_ball_direction = process.env.MOVE_IN_BALL_DIRECTION.split(",").map((n) => parseInt(n));
-        if (process.env.MOVE_TO_EXPECTED_POSITION != "none" && process.env.MOVE_TO_EXPECTED_POSITION != "all") this.move_to_expected_position = process.env.MOVE_TO_EXPECTED_POSITION.split(",").map((n) => parseInt(n));
+        if (tryCatchBall) {
+            if (tryCatchBall === "all") this.try_catch_the_ball_all = true;
+            if (tryCatchBall != "none" && tryCatchBall != "all") this.try_catch_the_ball = tryCatchBall.split(",").map((n) => parseInt(n));
+        }
 
-        console.log(`
-            ##########################################
-            try_catch_the_ball_all = ${this.try_catch_the_ball_all},
-            move_in_ball_direction_all = ${this.move_in_ball_direction_all},
-            move_to_expected_position_all = ${this.move_to_expected_position_all},
-            try_catch_the_ball = ${this.try_catch_the_ball},
-            move_in_ball_direction = ${this.move_in_ball_direction},
-            move_to_expected_position = ${this.move_to_expected_position},
-            ##########################################
-            TRY_CATCH_BALL = ${process.env.TRY_CATCH_BALL},
-            MOVE_IN_BALL_DIRECTION = ${process.env.MOVE_IN_BALL_DIRECTION},
-            MOVE_TO_EXPECTED_POSITION = ${process.env.MOVE_TO_EXPECTED_POSITION},
-            ##########################################
-        `);
+        if (moveInBallDirection) {
+            if (moveInBallDirection === "all") this.move_in_ball_direction_all = true;
+            if (moveInBallDirection != "none" && moveInBallDirection != "all") this.move_in_ball_direction = moveInBallDirection.split(",").map((n) => parseInt(n));
+        }
+
+        if (moveToExpectedPosition) {
+            if (moveToExpectedPosition === "all") this.move_to_expected_position_all = true;
+            if (moveToExpectedPosition != "none" && moveToExpectedPosition != "all") this.move_to_expected_position = moveToExpectedPosition.split(",").map((n) => parseInt(n));
+        }
     }
 
     shouldCatchTheBall(playerNumber: number): boolean {
